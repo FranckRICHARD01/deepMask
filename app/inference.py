c23d761ec076b4e438de79a150241f90f91096a2
Ravnoor Gill <ravnoor@gmail.com>
2021-02-28 21:27:18 -0500

    integrate deepMask with ants-based preprocessing

--- app/inference.py
+++ app/inference.py
@@ -1,19 +1,13 @@
 #!/usr/bin/env python3
-"""
-usage:
-inference.py ${id} ${id}_t1.nii.gz ${id}_flair.nii.gz ${IN_DIR} ${OUT_DIR}
-"""
 
-import os, sys, time
-
+import os
 import torch
-import torch.nn as nn
 from mo_dots import Data
 import multiprocessing
-
 from utils.data import *
 from deepmask import *
 import vnet
+from utils.image_processing import noelImageProcessor
 
 # configuration
 args = Data()
@@ -32,7 +26,7 @@ args.resize = (160,160,160)
 args.cuda = torch.cuda.is_available()
 
 torch.manual_seed(args.seed)
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+args.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 if args.cuda:
     torch.cuda.manual_seed(args.seed)
@@ -40,30 +34,8 @@ if args.cuda:
 else:
     print("build vnet, using CPU")
 
+model = vnet.build_model(args)
 
-def build_model(args):
-    # do not change these variable for inference
-    model = vnet.VNet(n_filters=6, outChans=2, elu=True, nll=True)
-    model = nn.parallel.DataParallel(model, device_ids=device)
+template = os.path.join('./template', 'mni_icbm152_t1_tal_nlin_sym_09a.nii.gz')
 
-    if os.path.isfile(args.inference):
-        print("=> loading checkpoint '{}'".format(args.inference))
-        if args.cuda:
-            checkpoint = torch.load(args.inference)
-        else:
-            checkpoint = torch.load(args.inference, map_location=torch.device('cpu'))
-        args.start_epoch = checkpoint['epoch']
-        model.load_state_dict(checkpoint['state_dict'])
-        print("=> loaded checkpoint '{}' (epoch {})"
-            .format(args.model, checkpoint['epoch']))
-    else:
-        sys.exit("=> no checkpoint found at '{}'".format(args.inference))
-
-    print('  + Number of params: {}'.format(
-        sum([p.data.nelement() for p in model.parameters()])))
-
-    if args.cuda:
-        print('moving the model to GPU')
-        model = model.cuda()
-
-    return model
+noelImageProcessor(id=str(case_id), t1=t1, t2=t2, output_dir=args.outdir, template=template, usen3=False, args=args, model=model).preprocessor()
